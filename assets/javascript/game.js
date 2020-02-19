05310bbf7f0c581a7b726e7bd1f6f550391de436
eozuna3 <eo30239@gmail.com>
2020-02-18 22:32:22 -0600

    completed most of the CSS and started to create variables and links to elements in CSS

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// jQuery methods go here...
+
+
+/*----------------------------Variables--------------------------------------------*/
+// Creating variables for use in javascript
+var completedgame = true;
+var losses = 0;
+var wins = 0;
+var selectedNumber = 0;
+var totalScore = 0;
+
+
+// Create variables that hold references to specific tags in the HTML where we want to display and change items.
+var lossesText = $("#losses");
+var winsText = $("#wins");
+var totalScoreText = $("#totalScore");
+var selectedNumberText = $("#selectedNumber");
+var button1 = $("#gemButton1");
+var button2 = $("#gemButton2");
+var button3 = $("#gemButton3");
+var button4 = $("#gemButton4");
+
